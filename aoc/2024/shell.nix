7cf796efc13a81130984b9e95e649f550d552796
Kierán Meinhardt <kmein@posteo.de>
2024-12-01 07:40:23 +0100

    2024: add script to determine which language to use

--- aoc/2024/shell.nix
+++ aoc/2024/shell.nix
@@ -1,4 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-  packages = [pkgs.lua];
+  packages = [
+    pkgs.lua
+
+    (pkgs.writers.writeDashBin "which-language" ''
+      ${pkgs.gnugrep}/bin/grep '^- \[ ]' README.md | ${pkgs.coreutils}/bin/shuf -n 1
+    '')
+  ];
 }
