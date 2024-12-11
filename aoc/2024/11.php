d43a6dd76b99b4b873aa1cc2fe3a3d97d75ea307
Kierán Meinhardt <kmein@posteo.de>
2024-12-11 12:58:56 +0100

    2024: day 11 in php (both parts, cheating)

--- aoc/2024/11.php
+++ aoc/2024/11.php
@@ -1,48 +1,33 @@
 <?php
-function splitNumber($number) {
-  $numberStr = (string)$number;
-  $midpoint = (int)(strlen($numberStr) / 2);
-  $leftHalf = substr($numberStr, 0, $midpoint);
-  $rightHalf = substr($numberStr, $midpoint);
-  return [intval($leftHalf), intval($rightHalf)];
-}
-
-function blink(array $numbers) {
-  $return = array();
-  foreach ($numbers as $number) {
-    if ($number == 0) {
-      array_push($return, 1);
-    } elseif (strlen((string)$number) % 2 == 0) {
-      $halves = splitNumber($number);
-      array_push($return, ...$halves);
-    } else {
-      array_push($return, $number * 2024);
+// stolen from https://www.reddit.com/r/adventofcode/comments/1hbm0al/comment/m1idchh/
+function blink($blinks, $numbers) {
+  $hash = array_count_values($numbers);
+  for ($i = 1; $i <= $blinks; $i++) {
+    $new = [];
+    foreach ($hash as $key => $value) {
+      if ($key == 0) {
+        $new[1] = ($new[1] ?? 0) + $value;
+      } elseif (strlen($key) % 2 == 0) {
+        $len = strlen($key) / 2;
+        $left = (int)substr($key, 0, $len);
+        $right = (int)substr($key, $len);
+        $new[$left] = ($new[$left] ?? 0) + $value;
+        $new[$right] = ($new[$right] ?? 0) + $value;
+      } else {
+        $new[$key * 2024] = ($new[$key * 2024] ?? 0) + $value;
+      }
     }
+    $hash = $new;
   }
-  return $return;
+  return array_sum($hash);
 }
-
-function blinkN(array $numbers, int $n) {
-  $return = $numbers;
-  for ($i = 0; $i < $n; $i++) {
-    echo "blink " . $i . PHP_EOL;
-    $return = blink($return);
-  }
-  return $return;
-}
-
-ini_set('memory_limit', '-1');
 
 $filename = getenv('AOC_TEST') ? '11.txt.test' : '11.txt';
-
-if (!file_exists($filename)) {
-  die("File not found: $filename");
-}
-
 $content = file_get_contents($filename);
-
 $numbers = array_map('intval', explode(' ', $content));
 
-echo count(blinkN($numbers, 25)) . PHP_EOL;
-echo count(blinkN($numbers, 75)) . PHP_EOL;
+$result = blink(25, $numbers);
+echo "$result\n";
+$result = blink(75, $numbers);
+echo "$result\n";
 ?>
